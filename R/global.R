720ae6ce07d6afaa1b685ef7c528a1808670451b
dataforyounz <taylor.rt17@gmail.com>
2023-04-30 20:10:27 +1200

    Included dataset filter in global file

--- R/global.R
+++ R/global.R
@@ -5,3 +5,9 @@ library( skimr )
 library( kableExtra )
 library( tidyverse )
 
+# Only want data frames
+
+datasets <- ls("package:datasets")
+data     <- lapply(datasets, get, "package:datasets")
+names    <- names[vapply(data, is.data.frame, logical(1))]
+
